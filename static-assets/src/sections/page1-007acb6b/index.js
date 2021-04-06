78de5bc6f57fd7ef97610e91ef1a3e1c22253cc5
admin admin <evaladmin@example.com>
2021-04-06 15:57:38 +0000

    User admin wrote content /static-assets/src/sections/page1-007acb6b/index.js

---
+++ static-assets/src/sections/page1-007acb6b/index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Ernesto  on 09/06/2017.
+ */
+
+(function(){
+    var component = Vue.component('page1', function (resolve, reject) {
+        Vue.http({url:locationPath+'src/sections/page1/template.html', method: 'GET'}).then(function (response) {
+            resolve({
+                template: response.data,
+                data: function () {
+                    return {
+                        name:'page1'
+                    }
+                },
+                created:function(){
+                    console.log('created:',this.name);
+
+
+                }
+            })
+        }, function (response) {
+            // error callback
+            console.error('ERROR_LOAD ');
+        });
+
+    });
+    window.router.addRoutes([{ path: '/page1', component: component }])
+})();
+
+
