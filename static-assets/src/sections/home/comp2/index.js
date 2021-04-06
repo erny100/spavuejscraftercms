1e2641a630b90cfaf6540961f16d2f8d3f851806
admin admin <evaladmin@example.com>
2021-04-06 15:51:31 +0000

    User admin wrote content /static-assets/src/sections/home/comp2/index.js

--- static-assets/src/sections/home/comp2/index.js
+++ static-assets/src/sections/home/comp2/index.js
@@ -4,7 +4,7 @@
 
 (function(){
     var component = Vue.component('comp2', function (resolve, reject) {
-        Vue.http({url:'src/sections/home/comp2/template.html', method: 'GET'}).then(function (response) {
+        Vue.http({url:locationPath+'src/sections/home/comp2/template.html', method: 'GET'}).then(function (response) {
             resolve({
                 template: response.data,
                 data: function () {
